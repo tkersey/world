67caf704b1cf26ef3bc398dded16664a9c694fa0
Tim Kersey <t@k-t.im>
2026-05-27 11:37:50 -0700

    fix: avoid running foreign target tests

--- build.zig
+++ build.zig
@@ -80,10 +80,11 @@ pub fn build(b: *std.Build) void {
         .filters = test_args.filters,
     });
     const test_step = b.step("test", "Run world tests.");
-    test_step.dependOn(&addRunArtifactWithArgs(b, tests, test_args.passthrough).step);
     if (target.query.isNative()) {
+        test_step.dependOn(&addRunArtifactWithArgs(b, tests, test_args.passthrough).step);
         b.default_step.dependOn(test_step);
     } else {
+        test_step.dependOn(&tests.step);
         b.default_step.dependOn(&tests.step);
     }
 
